c2498d7b62b49d6a0ae26cab2d404f2b16882f14
arctavares <cosilk@hotmail.com>
2023-09-22 00:57:41 -0300

    pega dados da api e mostra no card

--- src/componets/Cards/index.tsx
+++ src/componets/Cards/index.tsx
@@ -1,10 +1,87 @@
+import { useEffect, useState } from "react";
 import "./Cards.css";
+import axios from 'axios';
 
 export default function Cards() {
+
+    const [covidData, setCovidData] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [state, setState] = useState('RJ');
+
+    const allUF = [
+            'AC',
+            'AL' ,
+            'AP' ,
+            'AM' ,
+            'BA' ,
+            'CE' ,
+            'DF' ,
+            'ES' ,
+            'GO' ,
+            'MA' ,
+            'MT' ,
+            'MS' ,
+            'MG' ,
+            'PA' ,
+            'PB' ,
+            'PR' ,
+            'PE' ,
+            'PI' ,
+            'RJ' ,
+            'RN' ,
+            'RS' ,
+            'RO' ,
+            'RR' ,
+            'SC' ,
+            'SP' ,
+            'SE' ,
+            'TO'
+    ]
+
+    function returnAllUF () {
+        return allUF.map(uf => {
+            return <option>{uf}</option>
+        })
+    }
+
+    function handleChange (e:any) {
+        setState(e.target.value);
+        setLoading(true);
+        axios.get(`https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${e.target.value}`)
+            .then((res) => {
+                const data = res.data;
+                setCovidData(data);
+            })
+            .then(() => setLoading(false))
+    }
+
+    useEffect(() => {
+        setLoading(true);
+        axios.get(`https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/${state}`)
+            .then((res) => {
+                const data = res.data;
+                setCovidData(data);
+            })
+            .then(() => setLoading(false))
+    },[])
+
     return (
         <div className='cardsContainer'>
             <div className="covidInfoContainer card">
-                covid
+                {loading ? 'Loading...' : (
+                    <>
+                    <div className="selectRegion">
+                    <p>Confirmed cases</p>
+                    <select onChange={handleChange} value={state}>
+                        {returnAllUF()}
+                    </select>
+                </div>
+                <div className="numberOfCases">
+                    <h1>{covidData.cases}</h1>
+                </div>
+                </>
+                )}
+                
             </div>
             <div className="newsContainer card">
                 news
